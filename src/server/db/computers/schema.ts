97efc90a085b1294231f004a19e74bf004aff1dd
Creator X <90335627+Crea7orX@users.noreply.github.com>
2025-02-21 21:47:24 +0200

    feat: Create computers schema.ts

---
+++ src/server/db/computers/schema.ts
@@ -0,0 +1,25 @@
+import { sql } from "drizzle-orm";
+import {
+  boolean,
+  decimal,
+  integer,
+  pgTable,
+  varchar,
+} from "drizzle-orm/pg-core";
+import IdPrefix, { generateId } from "~/lib/ids";
+
+export const computers = pgTable("computers", {
+  id: varchar("id", { length: 256 })
+    .primaryKey()
+    .$default(() => generateId(IdPrefix.COMPUTER)),
+  usage: decimal("usage", { precision: 10, scale: 2 }).notNull(),
+  status: boolean("status").notNull().default(false),
+  lastTurnOnAt: integer("last_turn_on_at").notNull(),
+  ownerId: varchar("owner_id", { length: 256 }).notNull(),
+  createdAt: integer("created_at")
+    .default(sql`(EXTRACT(EPOCH FROM NOW()))`)
+    .notNull(),
+  updatedAt: integer("updated_at").$onUpdate(
+    () => sql`(EXTRACT(EPOCH FROM NOW()))`,
+  ),
+});
