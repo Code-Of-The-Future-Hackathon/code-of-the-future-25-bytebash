53d03e73f804a6bc53d1436453c5ab7606e693e3
Maximus019BG <maxralev019@gmail.com>
2025-02-21 22:15:27 +0200

    feat: new schema for TVs and absentees

---
+++ src/server/db/TVs/schema.ts
@@ -0,0 +1,25 @@
+import { sql } from "drizzle-orm";
+import {
+  boolean,
+  decimal,
+  integer,
+  pgTable,
+  varchar,
+} from "drizzle-orm/pg-core";
+import IdPrefix, { generateId } from "~/lib/ids";
+
+export const tv = pgTable("tv", {
+  id: varchar("id", { length: 256 })
+    .primaryKey()
+    .$default(() => generateId(IdPrefix.COMPUTER)),
+  name: varchar("name", { length: 256 }),
+  usage: decimal("usage", { precision: 10, scale: 2 }).notNull(),
+  type: varchar("type", { length: 256 }),
+  ownerId: varchar("owner_id", { length: 256 }).notNull(),
+  createdAt: integer("created_at")
+    .default(sql`(EXTRACT(EPOCH FROM NOW()))`)
+    .notNull(),
+  updatedAt: integer("updated_at").$onUpdate(
+    () => sql`(EXTRACT(EPOCH FROM NOW()))`,
+  ),
+});
---
+++ src/server/db/absentee/schema.ts
@@ -0,0 +1,20 @@
+import { sql } from "drizzle-orm";
+import {
+  boolean,
+  date,
+  decimal,
+  integer,
+  pgTable,
+  varchar,
+} from "drizzle-orm/pg-core";
+import IdPrefix, { generateId } from "~/lib/ids";
+
+export const absentee = pgTable("absentee", {
+  id: varchar("id", { length: 256 })
+    .primaryKey()
+    .$default(() => generateId(IdPrefix.COMPUTER)),
+  className: varchar("class_name", { length: 256 }),
+  absent: varchar("absent", { length: 256 }),
+  classStart: date("class-start"),
+  classEnd: date("class-start"),
+});
