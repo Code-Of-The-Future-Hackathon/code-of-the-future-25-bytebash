b1dd1b53be1687ef2b2b4e76a9374e2c2aa00981
Stoil100 <toli100ilov@gmail.com>
2025-02-21 21:52:57 +0200

    feat: Create app sidebar

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,11 +1,14 @@
 import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 import * as React from "react";
+import { AppSidebar } from "~/components/ui/app-sidebar";
+import { SidebarProvider } from "~/components/ui/sidebar";
 
 export default function AppLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <>
+    <SidebarProvider>
+       <AppSidebar />
       <SignedIn>
         {/*all main components here*/}
         {children}
@@ -13,6 +16,6 @@ export default function AppLayout({
       <SignedOut>
         <RedirectToSignIn />
       </SignedOut>
-    </>
+    </SidebarProvider>
   );
 }
