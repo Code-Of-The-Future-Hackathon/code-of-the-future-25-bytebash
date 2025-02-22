f65375b2c9053eb6b0cb38f38007c6baa04051d7
Creator X <90335627+Crea7orX@users.noreply.github.com>
2025-02-22 17:38:45 +0200

    style: Change body styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body className={""}>
+      <body className="relative min-h-dvh bg-background">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <ProvidersWithoutTheme>{children}</ProvidersWithoutTheme>
         </ThemeProvider>
