adeef19faaec76e8b9384b60d0516a0cea37d411
Creator X <90335627+Crea7orX@users.noreply.github.com>
2025-02-22 08:11:43 +0200

    feat: Load computers data from api

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -3,89 +3,89 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { type z } from "zod";
 import ComputerCard from "~/components/computers/card";
 import ComputerCreateDialog from "~/components/computers/dialog";
 import StatisticsPanel from "~/components/computers/statisctics";
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
 } from "~/components/ui/breadcrumb";
 import { Separator } from "~/components/ui/separator";
 import { SidebarInset, SidebarTrigger } from "~/components/ui/sidebar";
-import { computerCreateSchema, ComputerResponse } from "~/lib/validations/computer";
+import { useGetAllComputersQuery } from "~/hooks/api/use-get-all-computers-query";
+import {
+  computerCreateSchema,
+  type ComputerResponse,
+} from "~/lib/validations/computer";
 
 export default function Page() {
-    const [computers, setComputers] = useState<ComputerResponse[]>([]);
-    const [selectedComputer, setSelectedComputer] =
-        useState<ComputerResponse | null>(null);
+  const { data: computers, isLoading } = useGetAllComputersQuery({});
+  const [selectedComputer, setSelectedComputer] =
+    useState<ComputerResponse | null>(null);
 
-    const form = useForm<z.infer<typeof computerCreateSchema>>({
-        resolver: zodResolver(computerCreateSchema),
-        defaultValues: {
-            name: "",
-        },
-    });
+  const form = useForm<z.infer<typeof computerCreateSchema>>({
+    resolver: zodResolver(computerCreateSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
 
-    function onSubmit(values: z.infer<typeof computerCreateSchema>) {
-        console.log(values);
-        form.reset();
-    }
+  function onSubmit(values: z.infer<typeof computerCreateSchema>) {
+    console.log(values);
+    form.reset();
+  }
 
-    return (
-        <SidebarInset>
-            <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-                <SidebarTrigger className="-ml-1" />
-                <Separator orientation="vertical" className="mr-2 h-4" />
-                <Breadcrumb>
-                    <BreadcrumbList>
-                        <BreadcrumbItem className="hidden md:block">
-                            <BreadcrumbLink href="#">
-                                Computer Dashboard
-                            </BreadcrumbLink>
-                        </BreadcrumbItem>
-                        <BreadcrumbSeparator className="hidden md:block" />
-                        <BreadcrumbItem>
-                            <BreadcrumbPage>
-                                {selectedComputer
-                                    ? selectedComputer.name
-                                    : "Overview"}
-                            </BreadcrumbPage>
-                        </BreadcrumbItem>
-                    </BreadcrumbList>
-                </Breadcrumb>
-            </header>
-            <div className="flex flex-1 gap-4 p-4">
-                {computers.length === 0 ? (
-                    <ComputerCreateDialog form={form} onSubmit={onSubmit} />
-                ) : (
-                    <>
-                        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                            (
-                            {computers.map((computer) => (
-                                <ComputerCard
-                                    key={computer.id}
-                                    computer={computer}
-                                    onClick={() =>
-                                        setSelectedComputer(computer)
-                                    }
-                                />
-                            ))}
-                            )
-                        </div>
-                        <div className="flex-1">
-                            <StatisticsPanel
-                                computers={computers}
-                                selectedComputer={selectedComputer}
-                            />
-                        </div>
-                    </>
-                )}
+  if (isLoading || !computers) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <SidebarInset>
+      <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+        <SidebarTrigger className="-ml-1" />
+        <Separator orientation="vertical" className="mr-2 h-4" />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem className="hidden md:block">
+              <BreadcrumbLink href="#">Computer Dashboard</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator className="hidden md:block" />
+            <BreadcrumbItem>
+              <BreadcrumbPage>
+                {selectedComputer ? selectedComputer.name : "Overview"}
+              </BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </header>
+      <div className="flex flex-1 gap-4 p-4">
+        {computers.length === 0 ? (
+          <ComputerCreateDialog form={form} onSubmit={onSubmit} />
+        ) : (
+          <>
+            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+              {computers.map((computer) => (
+                <ComputerCard
+                  key={computer.id}
+                  computer={computer}
+                  onClick={() => setSelectedComputer(computer)}
+                />
+              ))}
             </div>
-        </SidebarInset>
-    );
+            <div className="flex-1">
+              <StatisticsPanel
+                computers={computers}
+                selectedComputer={selectedComputer}
+              />
+            </div>
+          </>
+        )}
+      </div>
+    </SidebarInset>
+  );
 }
