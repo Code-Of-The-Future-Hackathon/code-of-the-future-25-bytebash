c693a39a13da75fd6234ce342ba917aea28e1e9c
Creator X <90335627+Crea7orX@users.noreply.github.com>
2025-02-22 14:02:14 +0200

    feat: Create /api/computers/[id]/ DELETE route.ts

---
+++ src/app/api/computers/[id]/route.ts
@@ -0,0 +1,28 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { authenticate } from "~/lib/auth";
+import { handleError } from "~/lib/error";
+import { NotFoundError } from "~/lib/exceptions";
+import { computerResponseSchema } from "~/lib/validations/computer";
+import { computerDelete } from "~/server/db/computers/queries";
+
+interface IdProps {
+  params: Promise<{ id: string }>;
+}
+
+// delete device
+export async function DELETE(_request: NextRequest, props: IdProps) {
+  try {
+    const { id } = await props.params;
+    const { ownerId } = await authenticate();
+
+    const computer = await computerDelete({
+      id,
+      ownerId,
+    });
+    if (!computer) throw new NotFoundError();
+
+    return NextResponse.json(computerResponseSchema.parse(computer));
+  } catch (error) {
+    return handleError(error);
+  }
+}
