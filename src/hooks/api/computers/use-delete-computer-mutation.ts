3617c613056d76edbf32c7da4640fcb006dc6ea8
Creator X <90335627+Crea7orX@users.noreply.github.com>
2025-02-22 13:54:35 +0200

    fix: Delete endpoint in use-delete-computer-mutation.ts hook

--- src/hooks/api/computers/use-delete-computer-mutation.ts
+++ src/hooks/api/computers/use-delete-computer-mutation.ts
@@ -1,10 +1,7 @@
-import { axiosInstance } from "~/lib/axios";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { type AxiosError, type AxiosResponse } from "axios";
-import {
-  type ComputerCreate,
-  type ComputerResponse,
-} from "~/lib/validations/computer";
+import { axiosInstance } from "~/lib/axios";
+import { type ComputerResponse } from "~/lib/validations/computer";
 
 interface DeleteComputerMutationProps {
   id: string;
@@ -13,12 +10,9 @@ interface DeleteComputerMutationProps {
 export function useDeleteComputerMutation({ id }: DeleteComputerMutationProps) {
   const queryClient = useQueryClient();
 
-  return useMutation<
-    AxiosResponse<ComputerResponse>,
-    AxiosError
-  >({
+  return useMutation<AxiosResponse<ComputerResponse>, AxiosError>({
     mutationKey: ["Computers", "Delete"],
-    mutationFn: () => axiosInstance.delete(`/computers?${id}`),
+    mutationFn: () => axiosInstance.delete(`/computers/${id}`),
     onSuccess: () =>
       queryClient.invalidateQueries({
         queryKey: ["Computers", "GetAll"],
