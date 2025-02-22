19af8c338b33e8959cf6543724888f1bc2e25a74
Creator X <90335627+Crea7orX@users.noreply.github.com>
2025-02-22 12:54:59 +0200

    feat: Create use-delete-computer-mutation.ts hook

---
+++ src/hooks/api/computers/use-delete-computer-mutation.ts
@@ -0,0 +1,28 @@
+import { axiosInstance } from "~/lib/axios";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { type AxiosError, type AxiosResponse } from "axios";
+import {
+  type ComputerCreate,
+  type ComputerResponse,
+} from "~/lib/validations/computer";
+
+interface DeleteComputerMutationProps {
+  id: string;
+}
+
+export function useCreateComputerMutation({ id }: DeleteComputerMutationProps) {
+  const queryClient = useQueryClient();
+
+  return useMutation<
+    AxiosResponse<ComputerResponse>,
+    AxiosError,
+    ComputerCreate
+  >({
+    mutationKey: ["Computers", "Delete"],
+    mutationFn: () => axiosInstance.post(`/computers?${id}`),
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: ["Computers", "GetAll"],
+      }),
+  });
+}
