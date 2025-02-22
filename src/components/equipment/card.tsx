a99ed97f553f80df0e4258e1bad9e173b0cc32fa
Creator X <90335627+Crea7orX@users.noreply.github.com>
2025-02-22 21:45:14 +0200

    feat: Create equipment card.tsx component

--- src/components/equipment/card.tsx
+++ src/components/equipment/card.tsx
@@ -1,8 +1,15 @@
-import { Clock, Power } from "lucide-react";
+import { Clock, Power, Tag } from "lucide-react";
+import EquipmentDeleteAlert from "~/components/equipment/delete-alert";
 import { Badge } from "~/components/ui/badge";
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { cn } from "~/lib/utils";
 import { type EquipmentResponse } from "~/lib/validations/equipment";
-import TvDeleteAlert from "./delete-alert";
 
 export default function EquipmentCard({
   equipment,
@@ -12,40 +19,43 @@ export default function EquipmentCard({
   onClick: () => void;
 }) {
   return (
-    <div className={"w-full"}>
-      <Card
-        className="group cursor-pointer overflow-hidden transition-all duration-300 hover:shadow-md"
-        onClick={onClick}
+    <Card
+      className="group relative w-full max-w-[350px] cursor-pointer overflow-hidden transition-all duration-300"
+      onClick={onClick}
+    >
+      <Badge
+        className="absolute right-2 top-2 p-2"
+        variant={equipment.status ? "success" : "destructive"}
       >
-        <div className="absolute right-0 top-0 p-2">
-          <Badge variant={equipment.status ? "success" : "secondary"}>
-            {equipment.status ? "On" : "Off"}
-          </Badge>
+        {equipment.status ? "ON" : "OFF"}
+      </Badge>
+      <CardHeader>
+        <CardTitle className="flex items-center">
+          <Power
+            className={cn(
+              "mr-2 size-6 text-muted-foreground",
+              equipment.status && "text-success",
+            )}
+          />
+          <span>{equipment.name}</span>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col justify-end gap-2 text-muted-foreground">
+        <div className="flex items-center">
+          <Clock className="mr-1 size-6 shrink-0" />
+          Usage: {equipment.usage} hours
         </div>
-        <CardHeader className="pb-2">
-          <CardTitle className="flex items-center space-x-2">
-            <Power
-              className={`h-5 w-5 ${equipment.status ? "text-green-500" : "text-gray-400"}`}
-            />
-            <span>{equipment.name}</span>
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="space-y-2">
-            <p className="flex items-center text-sm text-muted-foreground">
-              <Clock className="mr-2 h-4 w-4" />
-              Usage: {equipment.usage} hours
-            </p>
-            <p className="text-sm text-muted-foreground">
-              Last Active: {new Date(equipment.updatedAt).toLocaleString()}
-            </p>
-            <div className="flex justify-end">
-              <TvDeleteAlert id={equipment.id} />
-            </div>
-          </div>
-        </CardContent>
-        <div className="h-1 w-full origin-left scale-x-0 transform bg-gradient-to-r from-primary to-primary/50 transition-transform duration-300 group-hover:scale-x-100" />
-      </Card>
-    </div>
+        <div className="flex items-center">
+          <Tag className="mr-1 size-6 shrink-0" />
+          Type: <Badge className="ml-1">{equipment.type}</Badge>
+        </div>
+      </CardContent>
+      <CardFooter>
+        <div className="absolute bottom-2 right-2">
+          <EquipmentDeleteAlert id={equipment.id} />
+        </div>
+      </CardFooter>
+      <div className="h-1 w-full origin-left scale-x-0 transform bg-gradient-to-r from-primary to-primary/50 transition-transform duration-300 group-hover:scale-x-100" />
+    </Card>
   );
 }
