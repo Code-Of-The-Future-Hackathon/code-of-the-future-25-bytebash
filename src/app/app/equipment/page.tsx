2023c7f9913ef705b46859ba86791b82c5462e0e
Creator X <90335627+Crea7orX@users.noreply.github.com>
2025-02-22 21:46:18 +0200

    feat: Create equipment page.tsx

--- src/app/app/equipment/page.tsx
+++ src/app/app/equipment/page.tsx
@@ -1,19 +1,9 @@
 "use client";
 
 import { useState } from "react";
-import TvCard from "~/components/equipment/card";
+import EquipmentCard from "~/components/equipment/card";
 import EquipmentCreateDialog from "~/components/equipment/dialog";
-import TvStatisticsPanel from "~/components/equipment/statisctics";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "~/components/ui/breadcrumb";
-import { Separator } from "~/components/ui/separator";
-import { SidebarInset, SidebarTrigger } from "~/components/ui/sidebar";
+import StatisticsPanel from "~/components/equipment/statistics";
 import { useGetAllEquipmentQuery } from "~/hooks/api/equipment/use-get-all-equipment-query";
 import { type EquipmentResponse } from "~/lib/validations/equipment";
 
@@ -27,53 +17,23 @@ export default function EquipmentPage() {
   }
 
   return (
-    <SidebarInset>
-      <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-        <SidebarTrigger className="-ml-1" />
-        <Separator orientation="vertical" className="mr-2 h-4" />
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem className="hidden md:block">
-              <BreadcrumbLink
-                href="#"
-                onClick={() => setSelectedEquipment(null)}
-              >
-                Equipment Dashboard
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator className="hidden md:block" />
-            <BreadcrumbItem>
-              <BreadcrumbPage>
-                {selectedEquipment ? selectedEquipment.name : "Overview"}
-              </BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
-      </header>
-      <div className="flex w-full flex-1 flex-col gap-4 p-4">
-        <h2 className="py-10 text-center text-3xl font-semibold tracking-tight">
-          Your Equipment
-        </h2>
-        <div className="grid w-full auto-rows-min gap-4 md:grid-cols-3">
-          <EquipmentCreateDialog />
-          {equipmentData.map((equipment) => (
-            <TvCard
-              key={equipment.id}
-              equipment={equipment}
-              onClick={() => setSelectedEquipment(equipment)}
-            />
-          ))}
-        </div>
-        <div className="flex-1">
-          <h2 className="py-10 text-center text-3xl font-semibold tracking-tight">
-            Statistics
-          </h2>
-          <TvStatisticsPanel
-            equipmentData={equipmentData}
-            selectedEquipment={selectedEquipment}
+    <div className="flex">
+      <div className="flex h-full w-full flex-wrap justify-center gap-4 p-4">
+        <EquipmentCreateDialog />
+        {equipmentData.map((equipment) => (
+          <EquipmentCard
+            key={equipment.id}
+            equipment={equipment}
+            onClick={() => setSelectedEquipment(equipment)}
           />
-        </div>
+        ))}
       </div>
-    </SidebarInset>
+      <div className="h-full w-full max-w-[360px] border-l bg-sidebar-border p-4 px-2">
+        <StatisticsPanel
+          equipmentData={equipmentData}
+          selectedEquipment={selectedEquipment}
+        />
+      </div>
+    </div>
   );
 }
