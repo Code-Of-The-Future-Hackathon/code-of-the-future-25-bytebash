97fb7763bec0111dffee5aae4a10bf8ba89ae77a
Creator X <90335627+Crea7orX@users.noreply.github.com>
2025-02-22 19:46:29 +0200

    feat: Create computers card.tsx component

--- src/components/computers/card.tsx
+++ src/components/computers/card.tsx
@@ -1,6 +1,13 @@
-import { Clock, Power } from "lucide-react";
+import { Calendar, Clock, Power } from "lucide-react";
 import { Badge } from "~/components/ui/badge";
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { cn } from "~/lib/utils";
 import type { ComputerResponse } from "~/lib/validations/computer";
 import ComputerDeleteAlert from "./delete-alert";
 
@@ -12,40 +19,43 @@ export default function ComputerCard({
   onClick: () => void;
 }) {
   return (
-    <div className={"w-full"}>
-      <Card
-        className="group cursor-pointer overflow-hidden transition-all duration-300 hover:shadow-md"
-        onClick={onClick}
+    <Card
+      className="group relative w-full max-w-[350px] cursor-pointer overflow-hidden transition-all duration-300"
+      onClick={onClick}
+    >
+      <Badge
+        className="absolute right-2 top-2 p-2"
+        variant={computer.status ? "success" : "destructive"}
       >
-        <div className="absolute right-0 top-0 p-2">
-          <Badge variant={computer.status ? "success" : "secondary"}>
-            {computer.status ? "On" : "Off"}
-          </Badge>
+        {computer.status ? "ON" : "OFF"}
+      </Badge>
+      <CardHeader>
+        <CardTitle className="flex items-center">
+          <Power
+            className={cn(
+              "mr-2 size-6 text-muted-foreground",
+              computer.status && "text-success",
+            )}
+          />
+          <span>{computer.name}</span>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col justify-end gap-2 text-muted-foreground">
+        <div className="flex items-center">
+          <Clock className="mr-1 size-6 shrink-0" />
+          Usage: {computer.usage} hours
         </div>
-        <CardHeader className="pb-2">
-          <CardTitle className="flex it ems-center space-x-2">
-            <Power
-              className={`h-5 w-5 ${computer.status ? "text-green-500" : "text-gray-400"}`}
-            />
-            <span>{computer.name}</span>
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="space-y-2">
-            <p className="flex items-center text-sm text-muted-foreground">
-              <Clock className="mr-2 h-4 w-4" />
-              Usage: {computer.usage} hours
-            </p>
-            <p className="text-sm text-muted-foreground">
-              Last Active: {new Date(computer.lastTurnOnAt).toLocaleString()}
-            </p>
-            <div className="flex justify-end">
-              <ComputerDeleteAlert id={computer.id} />
-            </div>
-          </div>
-        </CardContent>
-        <div className="h-1 w-full origin-left scale-x-0 transform bg-gradient-to-r from-primary to-primary/50 transition-transform duration-300 group-hover:scale-x-100" />
-      </Card>
-    </div>
+        <div className="flex items-center">
+          <Calendar className="mr-1 size-6 shrink-0" />
+          Last Active: {new Date(computer.lastTurnOnAt).toLocaleString()}
+        </div>
+      </CardContent>
+      <CardFooter>
+        <div className="absolute bottom-2 right-2">
+          <ComputerDeleteAlert id={computer.id} />
+        </div>
+      </CardFooter>
+      <div className="h-1 w-full origin-left scale-x-0 transform bg-gradient-to-r from-primary to-primary/50 transition-transform duration-300 group-hover:scale-x-100" />
+    </Card>
   );
 }
