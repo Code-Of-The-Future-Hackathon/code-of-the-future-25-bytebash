bd55d41fee92a60c7c1c47087c5aa55da8f4644f
Creator X <90335627+Crea7orX@users.noreply.github.com>
2025-02-22 08:43:24 +0200

    feat: Create /api/computers/ POST route.ts

--- src/app/api/computers/route.ts
+++ src/app/api/computers/route.ts
@@ -1,8 +1,12 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { authenticate } from "~/lib/auth";
 import { handleError } from "~/lib/error";
-import { computerResponseSchema } from "~/lib/validations/computer";
-import { computersGetAll } from "~/server/db/computers/queries";
+import {
+  ComputerCreate,
+  computerCreateSchema,
+  computerResponseSchema,
+} from "~/lib/validations/computer";
+import { computerInsert, computersGetAll } from "~/server/db/computers/queries";
 
 // get all computers
 export async function GET() {
@@ -14,6 +18,28 @@ export async function GET() {
     });
 
     return NextResponse.json(computerResponseSchema.array().parse(computers));
+  } catch (error) {
+    return handleError(error);
+  }
+}
+
+// create computer
+export async function POST(request: NextRequest) {
+  try {
+    const { ownerId } = await authenticate();
+
+    console.log("RUPAAAAAAAA", ownerId);
+    const json = (await request.json()) as ComputerCreate;
+    const create = computerCreateSchema.parse(json);
+
+    const computer = await computerInsert({
+      create,
+      ownerId,
+    });
+
+    return NextResponse.json(computerResponseSchema.parse(computer), {
+      status: 201,
+    });
   } catch (error) {
     return handleError(error);
   }
