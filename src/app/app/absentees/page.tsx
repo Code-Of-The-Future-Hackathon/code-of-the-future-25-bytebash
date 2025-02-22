5d269262bb7696caf03ec35567249cbaea9fc10f
Creator X <90335627+Crea7orX@users.noreply.github.com>
2025-02-22 22:28:11 +0200

    feat: Create absentee page.tsx

--- src/app/app/absentees/page.tsx
+++ src/app/app/absentees/page.tsx
@@ -2,19 +2,9 @@
 
 import { useState } from "react";
 import AbsenteeCard from "~/components/absentees/card";
-import AbsenteeStatisticsPanel from "~/components/absentees/statisctics";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "~/components/ui/breadcrumb";
-import { Separator } from "~/components/ui/separator";
-import { SidebarInset, SidebarTrigger } from "~/components/ui/sidebar";
+import AbsenteeStatisticsPanel from "~/components/absentees/statistics";
 import { useGetAllAbsenteesQuery } from "~/hooks/api/absentee/use-get-all-absentees-query";
-import { AbsenteeResponse } from "~/lib/validations/absentee";
+import { type AbsenteeResponse } from "~/lib/validations/absentee";
 
 export default function AbsenteesPage() {
   const { data: absentees, isLoading } = useGetAllAbsenteesQuery({});
@@ -25,41 +15,22 @@ export default function AbsenteesPage() {
     return <div>Loading...</div>;
   }
   return (
-    <SidebarInset>
-      <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-        <SidebarTrigger className="-ml-1" />
-        <Separator orientation="vertical" className="mr-2 h-4" />
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem className="hidden md:block">
-              <BreadcrumbLink href="#">Absentees Dashboard</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator className="hidden md:block" />
-            <BreadcrumbItem>
-              <BreadcrumbPage>
-                {selectedAbsentee ? selectedAbsentee.name : "Overview"}
-              </BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
-      </header>
-      <div className="flex flex-1 gap-4 p-4">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          {absentees.map((absentee) => (
-            <AbsenteeCard
-              key={absentee.id}
-              absentee={absentee}
-              onClick={() => setSelectedAbsentee(absentee)}
-            />
-          ))}
-        </div>
-        <div className="flex-1">
-          <AbsenteeStatisticsPanel
-            absentees={absentees}
-            selectedAbsentee={selectedAbsentee}
+    <div className="flex">
+      <div className="flex h-full w-full flex-wrap justify-center gap-4 p-4">
+        {absentees.map((absentee) => (
+          <AbsenteeCard
+            key={absentee.id}
+            absentee={absentee}
+            onClick={() => setSelectedAbsentee(absentee)}
           />
-        </div>
+        ))}
       </div>
-    </SidebarInset>
+      <div className="w-full max-w-[360px] border-l bg-sidebar-border p-4 px-2">
+        <AbsenteeStatisticsPanel
+          absentees={absentees}
+          selectedAbsentee={selectedAbsentee}
+        />
+      </div>
+    </div>
   );
 }
