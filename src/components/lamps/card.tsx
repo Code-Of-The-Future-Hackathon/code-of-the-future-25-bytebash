2b2e5108e4d7317b421667a6cb0e786b7c0e6739
Creator X <90335627+Crea7orX@users.noreply.github.com>
2025-02-22 21:25:39 +0200

    feat: Create lamps card.tsx component

--- src/components/lamps/card.tsx
+++ src/components/lamps/card.tsx
@@ -1,8 +1,15 @@
-import { Clock, Power } from "lucide-react";
+import { Calendar, Clock, Power, Tag } from "lucide-react";
+import LampDeleteAlert from "~/components/lamps/delete-alert";
 import { Badge } from "~/components/ui/badge";
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { cn } from "~/lib/utils";
 import type { LampsResponse } from "~/lib/validations/lamps";
-import LampDeleteAlert from "./delete-alert";
 
 export default function LampCard({
   lamp,
@@ -12,40 +19,50 @@ export default function LampCard({
   onClick: () => void;
 }) {
   return (
-    <div className="w-full">
-      <Card
-        className="group cursor-pointer overflow-hidden transition-all duration-300 hover:shadow-md"
-        onClick={onClick}
+    <Card
+      className="group relative w-full max-w-[360px] cursor-pointer overflow-hidden transition-all duration-300"
+      onClick={onClick}
+    >
+      <Badge
+        className="absolute right-2 top-2 p-2"
+        variant={lamp.status ? "success" : "destructive"}
       >
-        <div className="absolute right-0 top-0 p-2">
-          <Badge variant={lamp.status ? "success" : "secondary"}>
-            {lamp.status ? "On" : "Off"}
-          </Badge>
+        {lamp.status ? "ON" : "OFF"}
+      </Badge>
+      <CardHeader>
+        <CardTitle className="flex items-center">
+          <Power
+            className={cn(
+              "mr-2 size-6 text-muted-foreground",
+              lamp.status && "text-success",
+            )}
+          />
+          <span>{lamp.name}</span>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col justify-end gap-2 text-muted-foreground">
+        <div className="flex items-center">
+          <Tag className="mr-1 size-6 shrink-0" />
+          Group Name: <Badge className="ml-1">{lamp.groupName}</Badge>
         </div>
-        <CardHeader className="pb-2">
-          <CardTitle className="flex items-center space-x-2">
-            <Power
-              className={`h-5 w-5 ${lamp.status ? "text-green-500" : "text-gray-400"}`}
-            />
-            <span>{lamp.groupName}</span>
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="space-y-2">
-            <p className="flex items-center text-sm text-muted-foreground">
-              <Clock className="mr-2 h-4 w-4" />
-              Usage: {lamp.usage} hours
-            </p>
-            <p className="text-sm text-muted-foreground">
-              Last Active: {new Date(lamp.lastTurnOnAt).toLocaleString()}
-            </p>
-            <div className="flex justify-end">
-              <LampDeleteAlert id={lamp.id} />
-            </div>
-          </div>
-        </CardContent>
-        <div className="h-1 w-full origin-left scale-x-0 transform bg-gradient-to-r from-primary to-primary/50 transition-transform duration-300 group-hover:scale-x-100" />
-      </Card>
-    </div>
+        <div className="flex items-center">
+          <Clock className="mr-1 size-6 shrink-0" />
+          Usage: <span className="ml-1 text-primary">{lamp.usage} hours</span>
+        </div>
+        <div className="flex items-center">
+          <Calendar className="mr-1 size-6 shrink-0" />
+          Last Active:{" "}
+          <span className="ml-1 text-primary">
+            {new Date(lamp.updatedAt).toLocaleString()}
+          </span>
+        </div>
+      </CardContent>
+      <CardFooter>
+        <div className="absolute bottom-2 right-2">
+          <LampDeleteAlert id={lamp.id} />
+        </div>
+      </CardFooter>
+      <div className="h-1 w-full origin-left scale-x-0 transform bg-gradient-to-r from-primary to-primary/50 transition-transform duration-300 group-hover:scale-x-100" />
+    </Card>
   );
 }
