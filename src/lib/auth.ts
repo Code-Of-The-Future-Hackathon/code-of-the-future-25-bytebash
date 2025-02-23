14b8aabac44a4ee4cc860c820164c483efbb470c
Creator X <90335627+Crea7orX@users.noreply.github.com>
2025-02-23 02:03:35 +0200

    feat: Create option for auth.ts with api key

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,20 @@
 import { auth } from "@clerk/nextjs/server";
+import { type NextRequest } from "next/server";
 import { UnauthorizedError } from "~/lib/exceptions";
 
-export async function authenticate() {
+interface AuthenticateProps {
+  request?: NextRequest;
+}
+
+export async function authenticate({ request }: AuthenticateProps) {
+  if (request?.headers.get("X-API-KEY")) {
+    return {
+      apiKey: request.headers.get("X-API-KEY"),
+      userId: "",
+      ownerId: "",
+    };
+  }
+
   const authObject = await auth();
 
   if (!authObject.userId) {
