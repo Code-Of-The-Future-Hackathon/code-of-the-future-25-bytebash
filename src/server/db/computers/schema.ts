22795ab20f18fe6052ab7efc629b3b638ba53d64
Creator X <90335627+Crea7orX@users.noreply.github.com>
2025-02-23 02:04:02 +0200

    feat: Add api key field in computers schema.ts

--- src/server/db/computers/schema.ts
+++ src/server/db/computers/schema.ts
@@ -16,6 +16,7 @@ export const computers = pgTable("computers", {
   usage: decimal("usage", { precision: 10, scale: 2 }).notNull().default("0"),
   battery: integer("battery").notNull().default(0),
   status: boolean("status").notNull().default(false),
+  apiKey: varchar("api_key", { length: 256 }).notNull().default("pk_12345678"),
   lastTurnOnAt: integer("last_turn_on_at").notNull().default(0),
   ownerId: varchar("owner_id", { length: 256 }).notNull(),
   createdAt: integer("created_at")
